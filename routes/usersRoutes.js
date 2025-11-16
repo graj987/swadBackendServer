32e44edac2b37e7d18b6f1ea9b58d15dd5ae3b64
Mrgautam-git <gsharmapcmail9852@gmail.com>
2025-11-16 22:57:48 +0530

    update in profile

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser, loginUser, getUserProfile, updateUserProfile, getAllUsers, deleteUserById } from "../controllers/usersController.js";
+import { registerUser, loginUser, getUserProfile, updateUserProfile, getAllUsers, deleteUserById, getUserById } from "../controllers/usersController.js";
 
 const router = express.Router();
 
@@ -7,6 +7,7 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", getUserProfile);
 router.put("/profile", updateUserProfile);
+router.get("/:id", getUserById)
 router.get("/", getAllUsers);
 router.delete("/:id", deleteUserById);
 
